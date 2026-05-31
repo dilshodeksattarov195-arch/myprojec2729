8bc68b038cf9abbfc460efa6d301e01712af5e18
DILSHODBEK <dilshodsattar26@gmail.com>
2026-05-31 14:25:25 +0500

    Initial commit: setup main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+import sys
+import json
+
+class orderSetchWorker:
+    def __init__(self):
+        self.buffer = []
+    def append_metric(self, val):
+        self.buffer.append(val)
+        return len(self.buffer)
+
+if __name__ == '__main__':
+    obj = orderSetchWorker()
+    print("Worker engine initialized.")
